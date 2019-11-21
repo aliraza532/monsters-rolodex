7b7814ea08370ec34e05fb688f7485c910965d4e
unknown <532aliraza@gmail.com>
2019-11-21 18:07:34 +0500

    Updates

---
+++ precache-manifest.ebf686c32757b6250a6fdd91c7ce5b9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d916580ca49533c711b8e399fad45e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "804e0da67a8afa4eb694",
+    "url": "/monsters-rolodex/static/css/main.7b7e2c81.chunk.css"
+  },
+  {
+    "revision": "5bc7c05f66c3ef831429",
+    "url": "/monsters-rolodex/static/js/2.df32befe.chunk.js"
+  },
+  {
+    "revision": "804e0da67a8afa4eb694",
+    "url": "/monsters-rolodex/static/js/main.07100be9.chunk.js"
+  },
+  {
+    "revision": "075343449241dbe0ad6c",
+    "url": "/monsters-rolodex/static/js/runtime-main.58539e12.js"
+  }
+]);
